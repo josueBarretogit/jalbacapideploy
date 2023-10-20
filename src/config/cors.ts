eba305f81411d9897287671c098758f4339930ad
josueBarretogit <josuebarretogit@gmail.com>
2023-10-19 21:01:31 -0500

    datos credentials

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,9 +1,15 @@
 import * as cors from "cors";
 
-const allowedOrigins = [/http:\/\/localhost:\w/];
+const whitelist = ["https://anillosjalbac.com"];
 
 const corsOptions: cors.CorsOptions = {
-  origin: allowedOrigins[0],
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("No esta permitodo debido a CORS"));
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
--- src/data-source.ts
+++ src/data-source.ts
@@ -7,12 +7,13 @@ import Usuario from "./entity/Usuario";
 import { Solitario } from "./entity/Solitario";
 
 export const AppDataSource = new DataSource({
+  url: "sql3.freemysqlhosting.net",
   type: "mysql",
   host: "localhost",
   port: 3306,
-  username: "id21425586_josue",
-  password: process.env.DB_PASSWORD,
-  database: "id21425586_anillosjalbac",
+  username: "sql3654808",
+  password: "izv2VN57Hm",
+  database: " sql3654808",
   synchronize: false,
   logging: false,
   entities: [Anillo, Dije, Usuario, Solitario],
