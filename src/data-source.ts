a1ea5fde76e7815a0993cc77a1ea1bc0b6b0e6f8
josueBarretogit <josuebarretogit@gmail.com>
2023-10-20 11:53:24 -0500

    cambio database

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,13 +7,15 @@ import Usuario from "./entity/Usuario";
 import { Solitario } from "./entity/Solitario";
 
 export const AppDataSource = new DataSource({
-  url: "https://www.phpmyadmin.co/index.php",
+  url: "mysql://avnadmin:AVNS_nyfqata3GvAv_rykT_-@mysql-2eb00650-josueportela9-d77c.a.aivencloud.com:15131/defaultdb?ssl-mode=REQUIRED",
+
   type: "mysql",
-  host: "sql3.freemysqlhosting.net",
-  port: 3306,
-  username: "sql3654808",
-  password: "izv2VN57Hm",
-  database: " sql3654808",
+  host: "mysql-2eb00650-josueportela9-d77c.a.aivencloud.com",
+
+  port: 15131,
+  username: "avnadmin",
+  password: "AVNS_nyfqata3GvAv_rykT_-",
+  database: "defaultdb",
   synchronize: false,
   logging: false,
   entities: [Anillo, Dije, Usuario, Solitario],
