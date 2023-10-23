c5e3fe8c36670161f8d2b9ec173373342136945f
josueBarretogit <josuebarretogit@gmail.com>
2023-10-22 20:46:21 -0500

    prueba 1

--- src/data-source.ts
+++ src/data-source.ts
@@ -14,7 +14,7 @@ export const AppDataSource = new DataSource({
   username: "sql3654808",
   password: "izv2VN57Hm",
   database: "sql3654808",
-  synchronize: false,
+  synchronize: true,
   logging: false,
   entities: [Anillo, Dije, Usuario, Solitario],
   connectTimeout: 60 * 60 * 10000,
