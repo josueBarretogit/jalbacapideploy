91d9450aef95fed82016b4cff69a9fbdf8593260
josueBarretogit <josuebarretogit@gmail.com>
2023-10-22 21:11:06 -0500

    implementacion fs mkdir

---
+++ src/constanst/constanst.ts
@@ -0,0 +1,5 @@
+import path = require("path");
+
+const IMGFOLDER = path.join(__dirname, "public2");
+
+export { IMGFOLDER };
--- src/uploadImageConfig.ts
+++ src/uploadImageConfig.ts
@@ -1,12 +1,24 @@
 import { Request } from "express";
 import * as multer from "multer";
 import * as path from "path";
+import * as fs from "fs";
+import * as fsAsync from "fs/promises";
+import { IMGFOLDER } from "./constanst/constanst";
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    console.log(path.join(__dirname, "public2"));
-    cb(null, path.join(__dirname, "public2"));
+  destination: async (req, file, cb) => {
+    try {
+      console.log(IMGFOLDER);
+
+      if (!fs.existsSync(IMGFOLDER)) {
+        await fsAsync.mkdir(IMGFOLDER);
+      }
+      cb(null, IMGFOLDER);
+    } catch (error) {
+      console.log(error);
+    }
   },
+
   filename: (req, file, cb) => {
     const filename = file.originalname;
     cb(null, Date.now() + path.extname(filename));
