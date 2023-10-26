c289ba2533010fe3814f201cad299e1c180a8ab3
josueBarretogit <josuebarretogit@gmail.com>
2023-10-25 22:20:06 -0500

    fix(handlerMulter): fixed multerHanlderError

--- src/middleware/multerErroHandler.ts
+++ src/middleware/multerErroHandler.ts
@@ -13,8 +13,9 @@ export function handleMulterUpload(
       return res
         .status(500)
         .json({ response: "Hubo un error a la hora de subir la imagen" });
-    } else {
-      next();
+    } else if (error) {
+      return res.status(500).json({ response: "Hubo un error inesperado" });
     }
+    next();
   });
 }
