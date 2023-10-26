784ccae669fb9a8797ed6ec792f34b8fe57719cd
josueBarretogit <josuebarretogit@gmail.com>
2023-10-25 21:42:58 -0500

    fix(multerHanlder): arreglo multerHandler

--- src/middleware/multerErroHandler.ts
+++ src/middleware/multerErroHandler.ts
@@ -10,7 +10,7 @@ export function handleMulterUpload(
   next: NextFunction,
 ) {
   uploadMulterImage(req, res, (error: any) => {
-    if (req instanceof multer.MulterError) {
+    if (error instanceof multer.MulterError) {
       return res
         .status(500)
         .json({ response: "Hubo un error a la hora de subir la imagen" });
