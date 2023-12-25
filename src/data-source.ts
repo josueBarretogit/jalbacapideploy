4f753b80807cf788808c31f5cd078721ee34d82a
josueBarretogit <josuebarretogit@gmail.com>
2023-12-25 11:51:06 -0500

    cambios para usar bd postgres}

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,13 +7,13 @@ import Usuario from "./entity/Usuario";
 import { Solitario } from "./entity/Solitario";
 
 export const AppDataSource = new DataSource({
-  type: "mysql",
+  type: "postgres",
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  synchronize: false,
+  synchronize: true,
   logging: false,
   entities: [Anillo, Dije, Usuario, Solitario],
   migrations: [],
